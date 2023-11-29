c0ce0ba89ff153153c36f820da6730e9e47cb235
cwc020730 <xxzhangjinghexx@gmail.com>
2023-11-28 23:03:24 -0500

    Updated model architecture

--- rl_model.py
+++ rl_model.py
@@ -3,24 +3,24 @@ import torch.nn as nn
 import torch.optim as optim
 
 class RPSModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self):
         super(RPSModel, self).__init__()
-        # Define the architecture of the network
-        self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        # Define the architecture of the network
+        self.fc1 = nn.Linear(5, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 3)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
         return x
 
 if __name__ == '__main__':
-    # Example initialization
-    input_size = 2  # This can be adjusted based on how you define the state
-    hidden_size = 64  # Number of neurons in the hidden layer
-    output_size = 3  # Rock, Paper, Scissors
-
-    model = RPSModel(input_size, hidden_size, output_size)
-    print(model)
+    pass
